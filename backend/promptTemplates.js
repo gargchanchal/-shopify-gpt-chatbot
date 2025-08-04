f30d157d9f4fadb6216ad282e77a16b3882212c7
arnavdots <arnav.jain@dotsquares.com>
2025-08-04 16:08:45 +0530

    promptTemplates changes

---
+++ backend/promptTemplates.js
@@ -0,0 +1,8 @@
+// promptTemplates.js
+module.exports = {
+  systemPrompt: `
+You are a helpful assistant chatbot integrated into a Shopify store.
+You should answer all product-related inquiries and, if the user expresses purchase interest,
+send their details to HubSpot as a lead.
+`
+};
